2941c725efc2f7582a4839b407c51432088b66b5
QPing00 <qianping221039@dasein.edu.my>
2023-06-27 11:27:04 +0800

    week2 week3 completed

---
+++ Week 3/welcome_get.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+
+        Welcome <?php echo $_GET["name"]; ?><br>
+        Your email address is: <?php echo $_GET["email"]; ?>
+
+        <!-- [] is means that is an array  -->
+        <!-- can use vardump() to check array  -->
+        <!-- $GET become a temporary variable $_GET when it is in other page  -->
+
+    </body>
+</html>
