4646739ff75b85a40162782e91bec05f0fed1771
QPing00 <136405979+QPing00@users.noreply.github.com>
2023-07-02 19:27:02 +0800

    Update class_exercise_1.1.php

--- Week 3/class_exercise_1.1.php
+++ Week 3/class_exercise_1.1.php
@@ -12,8 +12,6 @@
                 echo ucwords(strtolower($_GET["f_name"] . " "));
                 echo ucwords(strtolower($_GET["l_name"])); 
             }
-
-            //if有value了（意思是有submit了 / $_GET == true），才会run这个if
         ?>
     </body>
 </html>
