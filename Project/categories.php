15678c4676d439d188ab746621bdd1c1a0dce6b7
QPing00 <qianping221039@dasein.edu.my>
2023-08-15 03:36:46 +0800

    add session & navigation bar

--- Project/categories.php
+++ Project/categories.php
@@ -1,8 +1,12 @@
+<?php
+include 'session.php';
+?>
+
 <!DOCTYPE HTML>
 <html>
 
 <head>
-    <title>PDO - Create a Category - PHP CRUD Tutorial</title>
+    <title>Create Categories</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -10,7 +14,10 @@
 </head>
 
 <body>
-    <!-- container -->
+    <?php
+    include 'navigation.php';
+    ?>
+
     <div class="container">
         <div class="page-header">
             <h1>Create Category</h1>
