42142a5370d68596f694bd49b9b259220fed0ce9
QPing00 <qianping221039@dasein.edu.my>
2023-09-12 07:30:34 +0800

    add dropdown

--- Project/navigation.php
+++ Project/navigation.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <style>
         .navbar {
             margin-bottom: 20px;
@@ -14,16 +16,56 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <div class="navbar-nav">
-                <a class="nav-link" href="dashboard.php">Dashboard</a>
-                <a class="nav-link" href="customer_read.php">Customers</a>
-                <a class="nav-link" href="product_read.php">Products</a>
-                <a class="nav-link" href="category_read.php">Category</a>
-                <a class="nav-link" href="order_read.php">Order</a>
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Customers
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="customer_read.php">Read Customers</a></li>
+                            <li><a class="dropdown-item" href="customer_create.php">Create Customers</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Products
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="product_read.php">Read Products</a></li>
+                            <li><a class="dropdown-item" href="product_create.php">Create Products</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Categories
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="category_read.php">Read Categories</a></li>
+                            <li><a class="dropdown-item" href="category_create.php">Create Categories</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Orders
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="order_read.php">Read Orders</a></li>
+                            <li><a class="dropdown-item" href="order_create.php">Create Orders</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
+
             <div class="ml-auto">
-                <a class="btn btn-secondary" href="logout.php">Logout</a>
+                <a class="btn btn-outline-light" href="logout.php">Logout</a>
             </div>
         </div>
     </nav>
