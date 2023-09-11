54e6cde3c223bd4025a47ef1923d2b57ed61ae2e
QPing00 <qianping221039@dasein.edu.my>
2023-09-12 07:31:29 +0800

    add delete

--- Project/order_read.php
+++ Project/order_read.php
@@ -23,7 +23,7 @@ include 'session.php';
 
     <div class="container">
         <div class="page-header">
-            <h1>Order List</h1>
+            <h1>Read Orders</h1>
         </div>
 
         <div class="d-flex justify-content-between mb-4">
@@ -40,7 +40,14 @@ include 'session.php';
         // include database connection
         include 'config/database.php';
 
-        $query = "SELECT os.order_id, os.username, c.first_name, c.last_name, os.order_date_time
+        // delete message prompt will be here
+        $action = isset($_GET['action']) ? $_GET['action'] : "";
+        // if it was redirected from delete.php
+        if ($action == 'deleted') {
+            echo "<div class='alert alert-success'>Record was deleted.</div>";
+        }
+
+        $query = "SELECT os.order_id, os.username, c.first_name, c.last_name, os.order_date_time, c.email
         FROM order_summary os
         INNER JOIN customers c ON os.username = c.username
         GROUP BY os.order_id
@@ -49,19 +56,20 @@ include 'session.php';
         // By grouping the rows using GROUP BY order_id, you tell the database to group the rows based on the order_id. 
         // The SUM function is then used to calculate the total price for each group, which corresponds to each unique order_id.
 
-        if ($_GET) {
+        if ($_GET && isset($_GET['search'])) {
             $search = $_GET['search'];
 
             if (empty($search)) {
                 echo "<div class='alert alert-danger'>Please enter a keyword</div>";
             }
 
-            $query = "SELECT os.order_id, os.username, c.first_name, c.last_name, os.order_date_time
+            $query = "SELECT os.order_id, os.username, c.first_name, c.last_name, os.order_date_time, c.email
             FROM order_summary os
             INNER JOIN customers c ON os.username = c.username
             WHERE os.username LIKE '%$search%'
             OR c.first_name LIKE '%$search%'
             OR c.last_name LIKE '%$search%'
+            OR c.email LIKE '%$search%'
             OR os.order_id LIKE '$search'
             GROUP BY os.order_id
             ORDER BY os.order_id DESC";
@@ -79,6 +87,7 @@ include 'session.php';
             echo "<th>Order ID</th>";
             echo "<th>Username</th>";
             echo "<th>Name</th>";
+            echo "<th>Email</th>";
             echo "<th>Order Date Time</th>";
             echo "<th>Total</th>";
             echo "<th>Action</th>";
@@ -96,6 +105,7 @@ include 'session.php';
                 $price_stmt->bindParam(':order_id', $order_id);
                 $price_stmt->execute();
 
+
                 $total = 0;
 
                 while ($price_row = $price_stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -108,6 +118,7 @@ include 'session.php';
                 echo "<td>{$order_id}</td>";
                 echo "<td>{$username}</td>";
                 echo "<td>$first_name $last_name</td>";
+                echo "<td>$email</td>";
                 echo "<td>{$order_date_time}</td>";
                 echo "<td class='text-end'>RM " . number_format($total, 2) . "</td>";
 
@@ -133,6 +144,18 @@ include 'session.php';
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
     <!-- confirm delete record will be here -->
+    <script type='text/javascript'>
+        // confirm record deletion
+        function delete_order(order_id) {
+
+            var answer = confirm('Are you sure?');
+            if (answer) {
+                // if user clicked ok,
+                // pass the id to delete.php and execute the delete query
+                window.location = 'order_delete.php?order_id=' + order_id;
+            }
+        }
+    </script>
 
 </body>
 
